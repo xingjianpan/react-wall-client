b5d6a87ac432cc01a926c9b872e0ad527b09d7c9
deric <github@mail.xingjian.me>
2017-03-10 14:58:46 +0800

    refactor App, Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
-import * as actions from '../actions';
+import * as actions from '../../actions';
 
 class Header extends Component {
 
