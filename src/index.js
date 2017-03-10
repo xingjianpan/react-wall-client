6ead34e067a5345fe275a6b1298daa15fdc5e3ed
deric <github@mail.xingjian.me>
2017-03-10 14:42:06 +0800

    after refactor store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router'
-import ReduxPromise from 'redux-promise';
-import reduxThunk from 'redux-thunk';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import requireAuth from './components/require_authentication';
-
-// mine
 import App from './components/App';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
@@ -15,25 +10,18 @@ import Signup from './components/auth/signup';
 import Resouces from './components/Resource';
 import PostList from './components/PostList';
 import AddPost from './components/AddPost';
-// reducers
-import reducers from './reducers';
 import * as actionTypes from './constants/actionTypes';
-
+import configureStore from './stores/configureStore';
 // css
 // import './index.css';
 
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise, reduxThunk)(createStore);
-const store = createStoreWithMiddleware(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  );
+const store = configureStore();
 const token = localStorage.getItem('token');
-
 // if we have a token, consider the use to be signin
 if (token) {
   // we need to update the application state
-  store.dispatch({type: actionTypes.AUTH_USER})
+  store.dispatch({ type: actionTypes.AUTH_USER });
 }
 
 
@@ -47,6 +35,7 @@ ReactDOM.render(
         <Route path="signup" component={Signup} />
         <Route path="resources" component={requireAuth(Resouces)} />
         <Route path="addpost" component={requireAuth(AddPost)} />
+        <Route path="*" component={PostList} />
       </Route>
     </Router>
   </Provider>
