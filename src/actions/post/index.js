28deb3e181e564d7a293bdda2171c3d2b8e2d37d
deric <github@mail.xingjian.me>
2017-03-14 14:04:35 +0800

    update service

--- src/actions/post/index.js
+++ src/actions/post/index.js
@@ -9,7 +9,7 @@ import { fetchPostList, setIgnoreLastFetch } from '../../actions/postlist';
 export const addPost = ({ title, content }) => {
   return (dispatch) => {
     axios.post(
-      `${POST_ROOT_URL}`,
+      `${POST_ROOT_URL}/`,
       { title, content },
       { headers: { Authorization: `Token ${localStorage.getItem('token')}`} },
     )
@@ -24,7 +24,7 @@ export const editPost = (post) => {
   const { id, title, content } = post;
   return (dispatch) => {
     axios.put(
-      `${POST_ROOT_URL}${id}/`,
+      `${POST_ROOT_URL}/${id}/`,
       { title, content },
       { headers: { Authorization: `Token ${localStorage.getItem('token')}` }},
     )
@@ -42,7 +42,7 @@ export const deletePost = (post) => {
   return (dispatch) => {
     //  The second parameter to axios.delete is config, not data
     axios.delete(
-      `${POST_ROOT_URL}${post.id}/`,
+      `${POST_ROOT_URL}/${post.id}/`,
       { headers: { Authorization: `Token ${localStorage.getItem('token')}`} },
     ).then(() => {
       browserHistory.push('/');
@@ -79,7 +79,7 @@ export const fetchPostItem = (id) => {
   return (dispatch) => {
     dispatch(postItemIsLoading(true));
 
-    axios.get(`${POST_ROOT_URL}${id}/`)
+    axios.get(`${POST_ROOT_URL}/${id}/`)
       .then(response => dispatch(fetchPostItemSuccess(response)))
       .catch(()=> dispatch(fetchPostItemFailed(true)));
   };
